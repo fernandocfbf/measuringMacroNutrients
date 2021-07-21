4054e2659d0b681e997dcdb68ae8891f99999bd4
unknown <fernando.fincatti@pay4fun01.local>
2021-07-21 14:19:00 -0300

    Adicionei o treinamento com a imagnet

---
+++ iteration-9/transfer_learning.py
@@ -0,0 +1,55 @@
+from numpy.core.arrayprint import printoptions
+from numpy.core.fromnumeric import size
+from sklearn.model_selection import train_test_split
+import keras
+from keras.applications.vgg16 import VGG16
+from keras.applications.vgg19 import VGG19
+from keras.applications.xception import Xception
+from keras.models import Model
+from keras.layers import Dropout, Flatten, Dense
+from load_images_cnn import *
+from keras import optimizers
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import argparse
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-m", "--model", default="VGG16", help="model to be reused")
+ap.add_argument("-s", "--size", default=64, help="image size")
+args = vars(ap.parse_args())
+
+# construct the image generator for data augmentation
+aug = ImageDataGenerator(rotation_range=30, width_shift_range=0.1, 
+                         height_shift_range=0.1, shear_range=0.2, zoom_range=0.2,
+                         horizontal_flip=True, fill_mode="nearest")
+
+size = int(args['size'])
+
+x, y = load_data(size) #loads images
+
+#divide between train and test data
+X_train, X_test, y_train, y_test =  train_test_split(x, y, test_size=0.33, random_state=84)
+
+print('Loading model {0}...'.format(args['model']))
+
+if args['model'].lower() == 'vgg16':
+    model = VGG16(input_shape=(size,size,3), include_top = False, weights= 'imagenet')
+elif args['model'].lower() == 'vgg19':
+    model = VGG19(input_shape=(size,size,3), include_top = False, weights= 'imagenet')
+elif args['model'].lower() == 'xception':
+    model = Xception(input_shape=(size,size,3), include_top = False, weights= 'imagenet')
+
+x = model.output
+x = Flatten()(x)
+x = Dense(3078,activation='relu')(x) 
+x = Dropout(0.5)(x)
+x = Dense(256,activation='relu')(x) 
+x = Dropout(0.2)(x)
+out = Dense(6,activation='softmax')(x)
+tf_model=Model(inputs=model.input,outputs=out)
+
+for layer in tf_model.layers[:20]:
+    layer.trainable=False
+
+tf_model.compile(optimizer=optimizers.Adam(), loss='categorical_crossentropy', metrics=['acc'])
+history = tf_model.fit(x=aug.flow(X_train, y_train, batch_size=32), epochs = 30, initial_epoch = 0, validation_data = (X_test, y_test), steps_per_epoch=len(X_train) // 32)
---
+++ readme.md
@@ -0,0 +1,2 @@
+To start using this repository, remember to clone the following one: https://github.com/fernandocfbf/measureData.
+Replace the folder name with "data"
